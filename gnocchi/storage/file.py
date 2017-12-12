c5c28a7e4c74e7de7c6db5cfa4a32657b1ef5118
Julien Danjou <julien@danjou.info>
2017-12-12 16:10:53 +0100

    file: move directory creation in the upgrade path, not init
    (cherry picked from commit 30d865e2b2c94ebf7884927bf9c2d849d29eed91)

--- gnocchi/storage/file.py
+++ gnocchi/storage/file.py
@@ -40,6 +40,8 @@ class FileStorage(_carbonara.CarbonaraBasedStorage):
         super(FileStorage, self).__init__(conf, incoming)
         self.basepath = conf.file_basepath
         self.basepath_tmp = os.path.join(self.basepath, 'tmp')
+
+    def upgrade(self, indexer):
         utils.ensure_paths([self.basepath_tmp])
 
     def _atomic_file_store(self, dest, data):
